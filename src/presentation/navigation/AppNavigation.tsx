4a598c6aed24e6f61e2f6438ff364feac4fb7c84
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2024-08-29 23:10:30 +0400

    Fixed status bars coloring

--- src/presentation/navigation/AppNavigation.tsx
+++ src/presentation/navigation/AppNavigation.tsx
@@ -5,6 +5,7 @@ import OutletDetailsScreen from "../features/outlet/details/OutletDetailsScreen"
 import CheckoutScreen from "../features/cart/CartCheckoutScreen";
 import OrderPlacedScreen from "../features/order/OrderPlacedScreen";
 import SearchScreen from "../features/search/SearchScreen";
+import {StatusBar} from "react-native";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -18,15 +19,24 @@ export type RootStackParamList = {
 
 const AppNavigation = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="OutletDetails" component={OutletDetailsScreen}/>
-                <Stack.Screen name="Checkout" component={CheckoutScreen}/>
-                <Stack.Screen name="OrderPlaced" component={OrderPlacedScreen}/>
-                <Stack.Screen name="Search" component={SearchScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <>
+            <StatusBar
+                barStyle={'dark-content'}
+                backgroundColor={"#f2f2f2"}
+            />
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{headerShown: false, navigationBarColor: "#f2f2f2"}}
+                    initialRouteName="Home"
+                >
+                    <Stack.Screen name="Home" component={HomeScreen}/>
+                    <Stack.Screen name="OutletDetails" component={OutletDetailsScreen}/>
+                    <Stack.Screen name="Checkout" component={CheckoutScreen}/>
+                    <Stack.Screen name="OrderPlaced" component={OrderPlacedScreen}/>
+                    <Stack.Screen name="Search" component={SearchScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
     )
 }
 
